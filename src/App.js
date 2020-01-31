d5a7a4886fb14318d09f4994fe1fd286a7f59383
Kihyun92 <okkihyun1@gmail.com>
2020-01-31 14:45:51 +0900

    리엑트 튜토리얼 7편

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
 import PhoneForm from "./components/PhoneForm";
-
+import PhoneInfoList from './components/PhoneInfoList';
 class App extends Component {
+    id = 2
+    state = {
+        information: [
+            {
+                id: 0,
+                name: '김기현',
+                phone: '010-5555-1111'
+            },
+            {
+                id: 1,
+                name: '스타벅스',
+                phone: '010-1234-0000'
+            }
+        ]
+    }
+
   handleCreate = data => {
-    console.log(data);
+    const {information} = this.state;
+    this.setState({
+        information: information.concat({id: this.id++, ...data})
+    })
   };
   
   render() {
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
+        <PhoneInfoList data={this.state.information}/>
       </div>
     );
   }
