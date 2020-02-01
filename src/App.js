2c5ca4405aee1c538ff43a5cba9855c286f75c1b
Kihyun92 <okkihyun1@gmail.com>
2020-02-01 15:40:25 +0900

    리엑트 튜토리얼 8편, 데이터 삭제 및 수정 작업 추가

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
 import React, { Component } from "react";
 import PhoneForm from "./components/PhoneForm";
-import PhoneInfoList from './components/PhoneInfoList';
+import PhoneInfoList from "./components/PhoneInfoList";
 class App extends Component {
-    id = 2
+    id = 2;
     state = {
         information: [
             {
                 id: 0,
-                name: '김기현',
-                phone: '010-5555-1111'
+                name: "김기현",
+                phone: "010-5555-1111"
             },
             {
                 id: 1,
-                name: '스타벅스',
-                phone: '010-1234-0000'
+                name: "스타벅스",
+                phone: "010-1234-0000"
             }
         ]
-    }
+    };
 
-  handleCreate = data => {
-    const {information} = this.state;
-    this.setState({
-        information: information.concat({id: this.id++, ...data})
-    })
-  };
-  
-  render() {
-    return (
-      <div>
-        <PhoneForm onCreate={this.handleCreate} />
-        <PhoneInfoList data={this.state.information}/>
-      </div>
-    );
-  }
+    handleCreate = data => {
+        const { information } = this.state;
+        this.setState({
+            information: information.concat({ id: this.id++, ...data })
+        });
+    };
+
+    handleRemove = id => {
+        const { information } = this.state;
+        this.setState({
+            information: information.filter(info => info.id !== id)
+        });
+    };
+
+    handleUpdate = (id, data) => {
+        const { information } = this.state;
+        this.setState({
+            information: information.map(info =>
+                info.id === id ? { ...info, ...data } : info
+            )
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <PhoneForm onCreate={this.handleCreate} />
+                <PhoneInfoList
+                    data={this.state.information}
+                    onRemove={this.handleRemove}
+                    onUpdate={this.handleUpdate}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/PhoneInfoList.js
+++ src/components/PhoneInfoList.js
@@ -1,20 +1,25 @@
-import React, {Component} from 'react';
-import PhoneInfo from './PhoneInfo';
+import React, { Component } from "react";
+import PhoneInfo from "./PhoneInfo";
 
 class PhoneInfoList extends Component {
     static defaultProps = {
-        data: []
-    }
-    
-    render() {
-        const {data} = this.props;
-        const list = data.map(
-            info => (<PhoneInfo key={info.id} info={info}/>)
-        );
+        data: [],
+        onRemove: () => console.warn("onRemove not defined"),
+        onUpdate: () => console.warn("onUpdate not defined")
+    };
 
-        return (
-            <div>{list}</div>
-        );
+    render() {
+        const { data, onRemove, onUpdate } = this.props;
+        const list = data.map(info => (
+            <PhoneInfo
+                key={info.id}
+                info={info}
+                onRemove={onRemove}
+                onUpdate={onUpdate}
+            />
+        ));
+
+        return <div>{list}</div>;
     }
 }
 
